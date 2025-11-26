ed90eeedb4c345147aff4255cb8a054ca4235c7d
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2025-11-25 22:24:18 -0300

    feat(lib): setup React Query provider with default options

--- lib/http.ts
+++ lib/http.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export const httpClient = axios.create({
-  baseURL: 'https://api.github.com',
+  baseURL: process.env.NEXT_PUBLIC_GITHUB_API_URL || 'https://api.github.com',
   headers: {
     'Accept': 'application/vnd.github.v3+json',
   },
+  timeout: 10000,
 });
